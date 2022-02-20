1b52c155020046406787ef007ae1120ca8b96f39
Jesus Fernandez <j.fernandez@blackringbusiness.com>
2022-02-19 22:27:24 -0300

    Send Message to RabbitMQ

--- WebApplication/SignalRooms/SignalRoom.cs
+++ WebApplication/SignalRooms/SignalRoom.cs
@@ -5,6 +5,8 @@ using WebApplication.Data.Entities;
 using WebApplication.Data.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using System.Linq;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace WebApplication.SignalRooms
 {
@@ -22,18 +24,53 @@ namespace WebApplication.SignalRooms
 
         public async Task SendMessage(string connectionID, string userId, string message)
         {
-            ChatHistory newChat = new ChatHistory()
+
+            if(message.Length > 0 && message.ToCharArray()[0] == '/')
             {
-                Message = message,
-                SenderId = userId,
-                TimeStamp = DateTime.Now
-            };
+                SendMessageToKiwi(message);
+            }
+            else
+            {
+                string userName = _userRepository.GetAll().Where(x => x.Id == userId).Select(x => x.UserName).FirstOrDefault();
 
-            _chatRepository.Insert(newChat);
+                ChatHistory newChat = new ChatHistory()
+                {
+                    Message = message,
+                    SenderId = userId,
+                    TimeStamp = DateTime.Now
+                };
 
-            string userName = _userRepository.GetAll().Where(x => x.Id == userId).Select(x => x.UserName).FirstOrDefault();
+                _chatRepository.Insert(newChat);
 
-            await Clients.AllExcept(connectionID).SendAsync("ReceivedMessage", userName, message);
+                await Clients.AllExcept(connectionID).SendAsync("ReceivedMessage", userName, message);
+            }
+
+            
+        }
+
+        //Kiwi is the name of the bot that will receive the message
+        public void SendMessageToKiwi(string message)
+        {
+            string _url = "amqps://pnceccsp:BFnfl8mUyG67F5oILrS6Z9PX5rfqdcDN@woodpecker.rmq.cloudamqp.com/pnceccsp";
+            string queueName = "kiwi";
+
+            var factory = new ConnectionFactory() { Uri = new Uri(_url) };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: queueName,
+                                     basicProperties: null,
+                                     body: body);
+            }
         }
     }
 }
